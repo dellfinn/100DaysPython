dc2fba6c72a422ddc8ac69d2b1f1182b4e7c5b2e
Italo Ruan <69631288+dellfinn@users.noreply.github.com>
2022-03-23 15:17:13 +0000

    Dia 3 completo

---
+++ Day002/desafio1.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+valor1 = int(two_digit_number[0])
+valor2 = int(two_digit_number[1])
+
+resultado_final = valor1 + valor2
+
+print (resultado_final)
